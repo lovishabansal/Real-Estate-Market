c8855041d6e676afc117cf6c32d1ed211db2d7ce
lovishabansal <122426842+lovishabansal@users.noreply.github.com>
2024-12-18 18:51:10 +0530

    Add files via upload

---
+++ backend/index.js
@@ -0,0 +1,54 @@
+import express, { application } from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
+import adminRouter from './routes/admin.route.js';
+import listingRouter from './routes/listing.route.js';
+import reportRouter from './routes/report.route.js';
+import cookieParser from 'cookie-parser';
+
+dotenv.config();
+
+// Connect application to MongoDB Database
+mongoose.connect(process.env.MONGO_CONN).then(() => {
+    console.log('Connected to MongoDB');
+}).catch((err) => {
+    console.log(err);
+});
+
+const app = express();
+
+// Allow JSON as input to the server otherwise it will get undefined
+app.use(express.json());
+
+// Initialize CookieParser
+app.use(cookieParser());
+
+const port = 3000;
+
+app.listen(port, () => {
+    console.log('Server is running on port',port);
+});
+
+// call at 3000/api/user
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/listing', listingRouter);
+app.use('/api/admin', adminRouter);
+app.use('/api/report', reportRouter);
+
+
+// Middleware
+app.use((err, req, res, next) => {
+    // 500 for Internal Server Error
+    const statusCode = err.statusCode || 500;
+    
+    const message = err.message || 'Internal Server Error';
+
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message
+    });
+});
---
+++ backend/models/user.model.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    avatar: {
+        type: String,
+        default: 'https://img.freepik.com/premium-vector/user-profile-icon-flat-style-member-avatar-vector-illustration-isolated-background-human-permission-sign-business-concept_157943-15752.jpg',
+    },
+}, { timestamps: true });
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
